ff49af7c6568ec0aa39c5221c12cb8ed6522a000
Ahyeon Jeon <ahyeonjeon@Ahyeons-MBP.attlocal.net>
2022-09-27 16:14:31 -0700

    add: eslint

--- src/index.js
+++ src/index.js
@@ -1,164 +1,162 @@
 // hello world -> Hello world
 function capitalize(str) {
-    // If empty string, return empty string
-    if (str.length === 0) {
-        return str
-    }
-    // First Character
-    const firstChr = str[0]
-    // Change the first character to upper case
-    const upperCase = firstChr.toUpperCase()
-    // All the letters other than the first letter
-    const otherChrs = str.slice(1)
+  // If empty string, return empty string
+  if (str.length === 0) {
+    return str;
+  }
+  // First Character
+  const firstChr = str[0];
+  // Change the first character to upper case
+  const upperCase = firstChr.toUpperCase();
+  // All the letters other than the first letter
+  const otherChrs = str.slice(1);
 
-    return upperCase+otherChrs
+  return upperCase + otherChrs;
 }
 
-console.log(capitalize('hello'))
+console.log(capitalize('hello'));
 
-// foo bar -> FOO BAR 
+// foo bar -> FOO BAR
 function allCaps(str) {
-    return str.toUpperCase()
+  return str.toUpperCase();
 }
 
-console.log(allCaps('foo bar'))
-
+console.log(allCaps('foo bar'));
 
 // do all the things -> Do All The Things
 function capitalizeWords(str) {
-    const wordsList = str.split(' ') // [do],[all],[the],[things]
-    const upperWords = wordsList.map( word => capitalize(word)) // upperWords = [Do], [All], ['The'], ['Things']
-    return upperWords.join(' ') // Do All The Things
+  const wordsList = str.split(' '); // [do],[all],[the],[things]
+  const upperWords = wordsList.map((word) => capitalize(word)); // [Do], [All], ['The'], ['Things']
+  return upperWords.join(' '); // Do All The Things
 }
 
-console.log(capitalizeWords('do all the things'))
+console.log(capitalizeWords('do all the things'));
 
 //    Hello    world! -> Hello world
 function removeExtraSpaces(str) {
-   const trimmedWords = str.trim()
-   const splitWords = trimmedWords.split(' ')
-   const filteredWords = splitWords.filter( (word) => word !== '' )
-   return filteredWords.join(' ')
+  const trimmedWords = str.trim();
+  const splitWords = trimmedWords.split(' ');
+  const filteredWords = splitWords.filter((word) => word !== '');
+  return filteredWords.join(' ');
 }
 
-console.log(removeExtraSpaces('   Hello    world!    '))
-
+console.log(removeExtraSpaces('   Hello    world!    '));
 
 // Hello world -> hello-world
 function kebobCase(str, separator = '-') {
-    const lowerWords = str.toLowerCase()
-    const splitChrs = lowerWords.split('')
-    const filterdWords = splitChrs.filter( (chr) => {
-        const code = chr.charCodeAt(0) // get character code for the chracterr at index zero
-        if (code > 96 && code < 123) { // keep letters 
-            return true
-        } else if (code > 47 && code < 58) { // keep numbers
-            return true
-        } else if (code === 32 || code === 45) { // keep hyphen
-            return true
-        }
-        return false
-    })
-    const joinedWords = filterdWords.join('')
-    // remove extra spaces
-    const spaceFree = removeExtraSpaces(joinedWords) //use removeExtraSpaces function to remove spaces
-    return spaceFree.split(' ').join(separator)
+  const lowerWords = str.toLowerCase();
+  const splitChrs = lowerWords.split('');
+  const filterdWords = splitChrs.filter((chr) => {
+    const code = chr.charCodeAt(0); // get character code for the chracterr at index zero
+    if (code > 96 && code < 123) { // keep letters
+      return true;
+    } if (code > 47 && code < 58) { // keep numbers
+      return true;
+    } if (code === 32 || code === 45) { // keep hyphen
+      return true;
+    }
+    return false;
+  });
+  const joinedWords = filterdWords.join('');
+  // remove extra spaces
+  const spaceFree = removeExtraSpaces(joinedWords);
+  return spaceFree.split(' ').join(separator);
 }
 
-console.log(kebobCase('Hello    world'))
+console.log(kebobCase('Hello    world'));
 
 //  what the heck  -> what_the_hack
 function snakeCase(str) {
-    return kebobCase(str, '_')
+  return kebobCase(str, '_');
 }
 
-console.log(snakeCase(' what the heck '))
-
+console.log(snakeCase(' what the heck '));
 
 // Camel Case -> camelCase
 function camelCase(str) {
-    const splitWords = str.split(' ') 
-    // loop over the words
-    const camelWords = splitWords.map( (word, i) => {
-        // loewrcase first word
-        if (i === 0) {
-            return word.toLowerCase()
-        }
-        else {
-            return capitalize(word)
-        }
-    })
-    return camelWords.join('')
+  const splitWords = str.split(' ');
+  // loop over the words
+  const camelWords = splitWords.map((word, i) => {
+  // loewrcase first word
+    if (i === 0) {
+      return word.toLowerCase();
+    }
+    return capitalize(word);
+  });
+  return camelWords.join('');
 }
 
-console.log(camelCase('Camel Case'))
-
+console.log(camelCase('Camel Case'));
 
 // Hello World -> ello WorldH
 function shift(str, shiftNum = 1) {
-    const splitWords = str.split('') // [Hello, World]
-    const numChrs = splitWords.map( (word, i) => {
-        if (i < shiftNum) {
-            return word
-        }
-    })
-    const Chrs = numChrs.join('') 
-    const slicedWord = str.slice(shiftNum).trim() // ello World
-    return slicedWord + Chrs
+  const splitWords = str.split(''); // [Hello, World]
+  // eslint-disable-next-line array-callback-return, consistent-return
+  const numChrs = splitWords.map((word, i) => {
+    if (i < shiftNum) {
+      return word;
+    }
+  });
+  const Chrs = numChrs.join('');
+  const slicedWord = str.slice(shiftNum).trim(); // ello World
+  return slicedWord + Chrs;
 }
 
-console.log(shift('foobar', 3))
-
+console.log(shift('foobar', 3));
 
 // Amazing bongo drums for sale -> ['#amazing', '#bongo', '#drums']
 function makeHashTag(str) {
-    const lowerWords = str.toLowerCase()
-    const splitWords = lowerWords.split(' ')
-    const wordsList = []
-    if (splitWords.length > 3) {
-        const sortedArr = splitWords.sort((a, b) => b.length - a.length); // descending order [#amazzing, #bongo, #drums, #sale, #for]
-        const threeWords = sortedArr.slice(0, 3)
-        for (word of threeWords) {
-            const poundedWord = '#'.concat(word)
-            wordsList.push(poundedWord)
-        }
+  const lowerWords = str.toLowerCase();
+  const splitWords = lowerWords.split(' ');
+  const wordsList = [];
+  if (splitWords.length > 3) {
+    const sortedArr = splitWords.sort((a, b) => b.length - a.length);
+    const threeWords = sortedArr.slice(0, 3);
+    // eslint-disable-next-line no-restricted-syntax, no-undef
+    for (word of threeWords) {
+      // eslint-disable-next-line no-undef
+      const poundedWord = '#'.concat(word);
+      wordsList.push(poundedWord);
     }
-    else {
-        for (word of splitWords) {
-            const poundedWord = '#'.concat(word)
-            wordsList.push(poundedWord)
-        }
+  } else {
+    // eslint-disable-next-line no-restricted-syntax, no-undef
+    for (word of splitWords) {
+      // eslint-disable-next-line no-undef
+      const poundedWord = '#'.concat(word);
+      wordsList.push(poundedWord);
     }
+  }
 
-    return wordsList
+  return wordsList;
 }
-console.log(makeHashTag('Amazing bongo drums zaswefaw for sale'))
+console.log(makeHashTag('Amazing bongo drums zaswefaw for sale'));
 
 // Abc def -> isEmpty("Abc def") // false
 function isEmpty(str) {
-    const trimmedWords = str.trim() // '   abc \t def '
-    if (trimmedWords.length === 0) {
-        return true
+  const trimmedWords = str.trim(); // '   abc \t def '
+  if (trimmedWords.length === 0) {
+    return true;
+  }
+  const splitChrs = trimmedWords.split('');
+  // eslint-disable-next-line no-restricted-syntax, no-undef
+  for (chr of splitChrs) {
+    // eslint-disable-next-line no-undef
+    if (chr !== ' ' && chr !== '\n' && chr !== '\t') {
+      return false;
     }
-    const splitChrs = trimmedWords.split('')
-    for (chr of splitChrs) {
-        if ( chr !== ' ' && chr !== '\n' && chr !== '\t'  ) {
-            return false
-        }
-    }
-    return true
+  }
+  return true;
 }
 
-console.log(isEmpty(' \tAbc def'))
+console.log(isEmpty(' \tAbc def'));
 
-module.exports.capitalize = capitalize
-module.exports.allCaps = allCaps
-module.exports.capitalizeWords = capitalizeWords
-module.exports.removeExtraSpaces = removeExtraSpaces
-module.exports.kebobCase = kebobCase
-module.exports.snakeCase = snakeCase
-module.exports.camelCase = camelCase
-module.exports.shift = shift
-module.exports.makeHashTag = makeHashTag
-module.exports.isEmpty = isEmpty
-
+module.exports.capitalize = capitalize;
+module.exports.allCaps = allCaps;
+module.exports.capitalizeWords = capitalizeWords;
+module.exports.removeExtraSpaces = removeExtraSpaces;
+module.exports.kebobCase = kebobCase;
+module.exports.snakeCase = snakeCase;
+module.exports.camelCase = camelCase;
+module.exports.shift = shift;
+module.exports.makeHashTag = makeHashTag;
+module.exports.isEmpty = isEmpty;
